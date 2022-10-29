ae3b74a4f360b227389d806e2bcdc41a3ac51291
Ilya Yudin <52705623+nibezo@users.noreply.github.com>
2022-10-29 15:59:32 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+for (let i = 1; i < 426; i++) {
+    document.getElementById('images').innerHTML += `<img src="https://veshok.com/dw/files/Memy/%D0%9C%D0%B5%D0%BC%20(${i}).jpg">`
+    console.log(i)
+}
