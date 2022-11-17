fbb8716cfaebee0978f60191f3c55007ec110d30
Ilya Yudin <52705623+nibezo@users.noreply.github.com>
2022-11-17 23:59:28 +0300

    извращение

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-for (let i = 1; i < 426; i++) {
-    document.getElementById('images').innerHTML += `<img src="https://veshok.com/dw/files/Memy/%D0%9C%D0%B5%D0%BC%20(${i}).jpg">`
-    console.log(i)
-}
+bad_ids = new Set();
+bad_ids_arr = []
+function idBadImgs(i) {
+    bad_ids.add(i);
+    bad_ids_arr = Array.from(bad_ids);
+};
+
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready() {
+    for (let i = 1; i < 425; i++) {
+        document.getElementById('images').innerHTML += `<img src="https://veshok.com/dw/files/Memy/%D0%9C%D0%B5%D0%BC%20(${i}).jpg" onerror="javascript: idBadImgs(${i})">`;
+    };
+};
+
